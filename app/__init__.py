d288b136027a20d6cdfa00d58993931bb809e30d
eunjaegu <kuhj1021@gmail.com>
2025-04-25 12:22:44 +0900

    ✨ 회원가입 기능 구현
    회원가입기능 추가 및  서버 실행을 위한 관련 파일 수정

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,28 @@
 # 앱 초기화 및 설정 파일
-from config import db
 from flask import Flask
+from config import db
 from flask_migrate import Migrate
+from app.routes import main_blp, user_blp
 
-import app.models
-
+# Migrate 객체를 전역 변수로 선언
 migrate = Migrate()
 
-
+# 팩토리 패턴
 def create_app():
+    # Flask 객체 생성 # 지역변수
     application = Flask(__name__)
 
+    # 환경 설정 로드
     application.config.from_object("config.Config")
     application.secret_key = "oz_form_secret"
 
+    # DB, 마이그레이션 초기화
     db.init_app(application)
-
     migrate.init_app(application, db)
 
-    # 이어서 블루 프린트 등록 코드를 작성해주세요!
+    # 블루 프린트 등록
+    application.register_blueprint(main_blp)
+    application.register_blueprint(user_blp)
 
+    # 지역변수 리턴
     return application
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,39 @@
-# 뷰 및 라우트 정의
+# # 뷰 및 라우트 정의
+from flask import request, jsonify, Blueprint
+from app.sevices.users import create_user
+
+# Blueprint 생성
+main_blp = Blueprint('Main', __name__, url_prefix='/main')
+user_blp = Blueprint('Users', __name__, url_prefix='/users')
+
+#기본 연결 확인
+@main_blp.route('/', methods = ['GET'])
+def index():
+    print("GET 요청 통과")
+    return jsonify({"message": "Success Connect"})
+
+#회원가입
+@user_blp.route('/', methods = ['POST'])
+# 회원가입(POST)
+def post():
+    print("라우트에 요청 도착")
+    user_data = request.get_json()
+    print(user_data)
+
+    try:
+         #create_user 함수 호출하여 새 사용자 생성
+        new = create_user(
+                name = user_data['name'], 
+                age = user_data['age'], 
+                gender = user_data['gender'], 
+                email = user_data['email']
+        )
+        #새 사용자 정보 반환
+        return jsonify({
+            "message": f"{new.name}님 회원가입을 축하합니다",
+            "user_id" : new.id
+        })
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    
+
